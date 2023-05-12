faeb451408a58ddb528849a21a5955f11bf69494
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-05-12 11:40:32 +0400

    Add packagin field to the product

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 
 from .views import (
     StatusListCreateAPIView, ProductTypeListCreateAPIView,
-    ProductModelViewSet, ProductHistoryListAPIView
+    ProductModelViewSet, ProductHistoryListAPIView,
+    PackageTypeListCreateAPIView
 )
 
 router = routers.DefaultRouter()
@@ -13,5 +14,6 @@ urlpatterns = [
     path('statuses/', StatusListCreateAPIView.as_view(), name="statuses"),
     path('products/types/', ProductTypeListCreateAPIView.as_view(), name="products_types"),
     path('products/history/', ProductHistoryListAPIView.as_view(), name="products_history"),
+    path('products/package/types/', PackageTypeListCreateAPIView.as_view(), name="package_types"),
     path('', include(router.urls))
 ]
