688d29113bf08836ccb2c309709730240490d319
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-05-12 14:26:01 +0400

    Add quantity

--- src/orders/models.py
+++ src/orders/models.py
@@ -58,6 +58,7 @@ class Product(models.Model):
         related_name="products",
         blank=True, null=True
     )
+    quantity = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
