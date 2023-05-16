c5531e065021a2117fc5ebd918cd2248533bbffa
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-05-16 14:18:51 +0400

    Add pagination class

--- src/trucks/views.py
+++ src/trucks/views.py
@@ -2,10 +2,12 @@ from rest_framework import generics, permissions
 from .serializers import TruckSerializer, TruckCreateSerializer
 from .models import Truck
 from .filters import TruckFilter
+from accounts.pagination import CustomPageNumberPagination
 
 
 class TruckListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticated,)
+    pagination_class = CustomPageNumberPagination
     queryset = Truck.objects.all().order_by('-id')
     filterset_class = TruckFilter
 
