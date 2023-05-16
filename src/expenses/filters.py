746a705b83628d7a288f3f57caab90c228134c18
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-05-16 11:53:23 +0400

    Add filter to expences

---
+++ src/expenses/filters.py
@@ -0,0 +1,15 @@
+from django_filters import rest_framework as dj_filters
+from .models import Expense
+
+
+class ExpenseFilter(dj_filters.FilterSet):
+    start_created_at = dj_filters.DateFilter(field_name="date", lookup_expr="gte")
+    end_created_at = dj_filters.DateFilter(field_name="date", lookup_expr="lte")
+
+    class Meta:
+        model = Expense
+        fields = (
+            "type",
+            "start_created_at",
+            "end_created_at"
+        )
--- src/expenses/views.py
+++ src/expenses/views.py
@@ -2,6 +2,7 @@ from .models import Expense, Type
 from rest_framework import viewsets, generics, permissions
 from .serializers import TypeSerializer, ExpenseCreateUpdateSerializer, ExpenseSerializer
 from accounts.pagination import CustomPageNumberPagination
+from .filters import ExpenseFilter
 
 
 class TypeListCreateAPIView(generics.ListCreateAPIView):
@@ -13,7 +14,7 @@ class ExpenseModelViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     pagination_class = CustomPageNumberPagination
     queryset = Expense.objects.all()
-    filterset_fields = ('date',)
+    filterset_class = ExpenseFilter
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
