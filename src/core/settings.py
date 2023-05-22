a946cf712a6b6bfa2a67f77f1c3764b5013e5c00
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-05-22 13:46:57 +0400

    Change lifetime changes

--- src/core/settings.py
+++ src/core/settings.py
@@ -141,8 +141,8 @@ REST_FRAMEWORK = {
 # Simple JWT
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
