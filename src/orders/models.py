8ec195c770623f988e2fd015a016ab35ca744fc2
Kaiduev <kaiduevbaktygul07@gmail.com>
2023-07-02 16:39:57 +0600

    Add position field for product type

--- src/orders/models.py
+++ src/orders/models.py
@@ -24,6 +24,7 @@ class PaymentStatus(models.Model):
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255, unique=True)
+    position = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
